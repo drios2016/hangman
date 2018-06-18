12b546501f2d5f030b85a046750090443b4a0175
drios2016 <drios@ucmerced.edu>
2018-06-18 12:57:41 -0700

    Update hangman.py
    Fixed formatting, added "blanks" method to initialize list of blanks (letters)

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -2,9 +2,9 @@ from random import randint
 import time
   
 def takeBreak(sec):
-  for i in range(0,sec):
-    print "..."
-    time.sleep(1)
+	for i in range(0,sec):
+		print "..."
+		time.sleep(1)
     
 def chooseWord():
   """num = 0
@@ -14,72 +14,76 @@ def chooseWord():
   word = file.readline(randint(0,num)):
   """
   
-  word = 'bagpipes'
-  return word
+	word = 'bagpipes'
+	return word
   
 def printFig(letters):
-  lString = ''
-  for i in range (0, len(letters)):
-    lString = lString + letters[i]
-  print lString
+	lString = ''
+	for i in range (0, len(letters)):
+		lString = lString + letters[i]
+	print lString
     
 def goodGuess(guess, word, letters):
-  for i in range(0, len(word)):
-    if word[i] == guess:
-      letters[i] = guess + ' '
-  return letters
+	for i in range(0, len(word)):
+		if word[i] == guess:
+			letters[i] = guess + ' '
+	return letters
   
 def guessWord(word, chances):
-  print "Would you like to guess the word? Y/N"
-  ans = input()
-  if (ans == 'Y'):
-    print "What's your guess?"
-    wGuess = input()
-    if (wGuess == word):
-      print "You got it!"
-      return True, chances
-    else:
-      chances = chances - 1
-      print "Nope, nice try! You have ", chances, " chances left!"
-      return False, chances
-  else:
-    print "Okay!"
-    return False, chances
-  
-def play(word, chances):
-  letters = []
-  for i in range (0, len(word)):
-    letters.append("_ ")
-    
-  print "The word has ", len(word), " letters!"
+	print "Would you like to guess the word? Y/N"
+	ans = input()
+	if (ans == 'Y'):
+		print "What's your guess?"
+		wGuess = input()
+			if (wGuess == word):
+				print "You got it!"
+				return True, chances
+			else:
+				chances = chances - 1
+				print "Nope, nice try! You have ", chances, " chances left!"
+				return False, chances
+	else:
+		print "Okay!"
+		return False, chances
 
-  win = False
-  while(chances > 0 and '_ ' in letters and win == False):
-    printFig(letters)
-    print "Guess a letter!"
-    guess = input()
-    if (guess in word):
-      print "Correct!"
-      letters = goodGuess(guess, word, letters)
-      win, chances = guessWord(word, chances)
-    else:
-      chances = chances - 1
-      print "Oooh, sorry! You have ", chances, " chances left!"
-      win, chances = guessWord(word, chances)
-      
-  if ('_ ' not in letters or win == True):
-    print "You won! The word was: ", word
-  elif (chances == 0):
-    print "Boo...maybe next time? The word was: ", word
-    
+def blanks(word):
+	letters = []
+	for i in range (0, len(word)):
+		letters.append("_ ")
+	return letters
+  
+def play():
+	word = chooseWord()
+	letters = blanks(word)
+    print "Kay! I've chosen a word! It has ", len(word), " letters!"
+    print "You have 7 chances! Let's play!" 
+
+	win = False
+	chances = 7
+	while(chances > 0 and '_ ' in letters and win == False):
+		printFig(letters)
+		print "Guess a letter!"
+		guess = input()
+		if (guess in word):
+			print "Correct!"
+			letters = goodGuess(guess, word, letters)
+			win, chances = guessWord(word, chances)
+		else:
+			chances = chances - 1
+			print "Oooh, sorry! You have ", chances, " chances left!"
+			win, chances = guessWord(word, chances)
+  
+	if ('_ ' not in letters or win == True):
+		print "You won! The word was: ", word
+	elif (chances == 0):
+		print "Boo...maybe next time? The word was: ", word
+
+#####MAIN#####
 while (1):
   print "Would you like to play hangman? Y/N"
   ans = input()
   if (ans == 'Y'):
-    word = chooseWord()
-    chances = 7
-    print ("Kay! I've chosen a word! You have 7 chances, let's play!")
-    play(word, chances)
+    play()
   elif (ans == 'N'):
     print ("Aw, kay.")
     takeBreak(randint(1,5))
